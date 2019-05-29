1d67ef5497cce1d3ac6234fb9a7c9239ecb2e989
Sherab Sangpo Dorje <po@poism.com>
2019-05-29 02:51:28 -0400

    Lots of work, centralize all API information into cloudflare.php, generate forms via functions, add full_zone_set and zone_list

--- cloudflare.php
+++ cloudflare.php
@@ -2,19 +2,157 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-
 class CloudFlare {
+	// Built by po@poism.com based on https://www.cloudflare.com/docs/host-api/
 	private $HOST_GW_SERVICE_URL;
-	private $ENABLE_CLOBBER_UNIQUE_ID;
 	private $HOST_KEY;
+	public 	$ENABLE_CLOBBER_UNIQUE_ID;
+	public 	$actions;
+	public	$fields;
 	public 	$returnJson = true;
 
-	function __construct($hostKey) {
+	function __construct() {
 		$this->HOST_GW_SERVICE_URL = 'https://api.cloudflare.com/host-gw.html';
 		$this->ENABLE_CLOBBER_UNIQUE_ID = true;
+		$this->HOST_KEY = false;
+
+		// List of API Actions
+		$this->actions=array(
+			"user_create"=>array(
+				"title"=>"Create User",
+				"description"=>"Create a CloudFlare account for a user. Register you as the host provider.",
+				"required"=>array("cloudflare_email","cloudflare_pass"),
+				"optional"=>array("cloudflare_username","unique_id")
+			),
+			"user_lookup"=>array(
+				"title"=>"Lookup User",
+				"description"=>"Lookup a user by 'cloudflare_email' or by a previously assigned 'unique_id'.",
+				"optional"=>array("cloudflare_email","unique_id")
+			),
+			"user_auth"=>array(
+				"title"=>"Authorize User",
+				"description"=>"Authorize you as the host provider for an existing user.",
+				"required"=>array("cloudflare_email","cloudflare_pass"),
+				"optional"=>array("unique_id")
+			),
+			"zone_list"=>array(
+				"title"=>"List Zones",
+				"description"=>"List the domains currently active on Cloudflare for the given host.",
+				"optional"=>array("zone_name")
+			),
+			"zone_lookup"=>array(
+				"title"=>"Lookup Zone",
+				"description"=>"Lookup a Zone by 'user_key' and 'zone_name'.",
+				"required"=>array("user_key","zone_name")
+			),
+			"zone_set"=>array(
+				"title"=>"Setup Zone",
+				"description"=>"Add a Zone using the CNAME method. Can use your own nameservers.",
+				"required"=>array("user_key","zone_name","resolve_to","subdomains")
+			),
+			"full_zone_set"=>array(
+				"title"=>"Setup Zone Fully",
+				"description"=>"Add a Zone using the full setup method. Requires Cloudflare nameservers.",
+				"required"=>array("user_key","zone_name")
+			),
+			"zone_delete"=>array(
+				"title"=>"Delete Zone",
+				"description"=>"Delete a Zone by 'user_key' and 'zone_name'.",
+				"required"=>array("user_key","zone_name")
+			)
+		);
+
+
+		// List of API Field Definitions
+		$this->fields=array(
+			"cloudflare_username"=>array(
+				"label"=>"Cloudflare Username",
+				"type"=>"text"
+			),
+			"cloudflare_email"=>array(
+				"label"=>"Cloudflare Email",
+				"type"=>"email"
+			),
+			"cloudflare_pass"=>array(
+				"label"=>"Cloudflare Password",
+				"type"=>"password"
+			),
+			"unique_id"=>array(
+				"label"=>"Unique ID",
+				"tooltip"=>"A unique string identifying the user, serves as an alias to their Cloudflare account for your system.",
+				"type"=>"text"
+			),
+			"user_key"=>array(
+				"label"=>"User Key",
+				"tooltip"=>"Unique 32 hex char auth string identifying the Cloudflare user account. Generated from user_create or user_auth.",
+				"type"=>"password"
+			),
+			"zone_name"=>array(
+				"label"=>"Zone Name",
+				"tooltip"=>"The zone you wish to run CNAMES through, eg. somedomain.com",
+				"type"=>"text"
+			),
+			"resolve_to"=>array(
+				"label"=>"Resolve To",
+				"tooltip"=>"CNAME to resolve filtered traffic to, eg. cloudflare-resolve-to.somedomain.com",
+				"type"=>"text"
+			),
+			"subdomains"=>array(
+				"label"=>"Subdomains",
+				"tooltip"=>"Comma-separated string of subdomains for Cloudflare to host eg. www,blog,wordpress",
+				"type"=>"text"
+			)
+		);
+		
+
+	}
+	function setHostKey($hostKey) {
 		$this->HOST_KEY=$hostKey;
 	}
 
+	function submitAction($action, $data) {
+		// This function replaces all of the other userCreate, userLookup, etc.
+
+		//$action = filter_var($_POST['action'], FILTER_SANITIZE_STRING);
+
+		if (! array_key_exists($action, $this->actions)) {
+			return $this->processResponse(FALSE, "Requested ".$action." action not supported.");
+		}
+
+		$required = isset($this->actions[$action]['required']) ? $this->actions[$action]['required'] : [];
+		$optional = isset($this->actions[$action]['optional']) ? $this->actions[$action]['optional'] : [];
+		//$requiredVals=array_intersect_key($data,$required);
+		//$optionalVals=array_intersect_key($data,$optional);
+
+		$fields = array_merge($required, $optional);
+		$params = array( 'act'=>$action );
+		foreach ($data as $d=>$v) {
+			if (!in_array($d, $fields)) continue; //skip irrelevant data
+			if (@$this->fields[$d]['type'] == 'email') $params[$d] = filter_var($v, FILTER_SANITIZE_EMAIL);
+			elseif (@$this->fields[$d]['type'] == 'text') $params[$d] = filter_var($v, FILTER_SANITIZE_STRING);
+			elseif (@$this->fields[$d]['type'] == 'password') $params[$d] = $v;
+
+			if (empty($params[$d])) unset($params[$d]);
+		}
+
+		if ( $action=="user_lookup" && empty($required) && count($params)<=1) {
+			//user_lookup for instance has no required fields, but at least needs something
+			return $this->processResponse(FALSE, "Insufficient fields for ".$action." provided."); 
+		}
+		if ( !empty( array_diff($required, array_keys($params)) ) ) {
+			return $this->processResponse(FALSE, "Required fields for ".$action." were missing.");
+		}
+
+		foreach ($optional as $o) { //doubt we need to send null on undefined optional values, but doing it since the sample api did so
+			if (!isset($params[$o])) $params[$o] = NULL;
+		}
+		
+		return $this->processResponse( $this->performRequest($params) );
+	}
+
+	//
+	// FIXME: all of the following API actions are replaced by the previous sendAction function
+	//
 
 	function userCreate($cfEmail,$cfPass,$cfUsername='',$uniqueId='') {
 	// Create a CloudFlare account for a user. Register you as the host provider.
@@ -23,7 +161,7 @@ class CloudFlare {
     		$params["cloudflare_email"]     = filter_var($cfEmail, FILTER_SANITIZE_EMAIL);
     		$params["cloudflare_pass"]      = $cfPass;
     		// optional:
-		$params["cloudflare_username"]  = empty($cfUsername) ? NULL : filter_var($cfUsername, FILTER_SANTIZE_STRING);
+		$params["cloudflare_username"]  = empty($cfUsername) ? NULL : filter_var($cfUsername, FILTER_SANITIZE_STRING);
     		$params["unique_id"]            = empty($uniqueId) ? NULL : filter_var($uniqueId, FILTER_SANITIZE_STRING);
 
 		return $this->processResponse( $this->performRequest($params) );
@@ -88,6 +226,8 @@ class CloudFlare {
 	private function performRequest(& $data, $headers=NULL) {
 	// Contact the service. Returns FALSE on error.
 
+		if ( !$this->HOST_KEY ) return FALSE; //fixme add better fail messages
+
 		$data["host_key"] = $this->HOST_KEY;
 	
 		$ch = curl_init();
@@ -109,8 +249,8 @@ class CloudFlare {
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, TRUE);
 		
 		if (($http_result = curl_exec($ch)) === FALSE) {
-			echo "WARNING: A connectivity error occured while contacting the service.\n";
-			trigger_error(curl_error($ch));
+			//echo "WARNING: A connectivity error occured while contacting the service.\n";
+			//trigger_error(curl_error($ch));
 			return FALSE;
 		}
 		
--- index.php
+++ index.php
@@ -17,31 +17,73 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-if ($_SERVER['REMOTE_ADDR'] !== "FIXME") {
-	echo "<h1>INVALID IP</h1>";
+if ($_SERVER['REMOTE_ADDR'] !== "FIXME" || !isset($_REQUEST['FIXME'])) {
+	echo "<h1>Nothing to see here...</h1>";
 	die();
 }
 
+require_once("cloudflare.php");
+$CF = new CloudFlare();
+
 function getSubmitResults() {
+	global $CF;
 	if (count($_POST) == 0) return "No Results Yet";
 
-	require_once("cloudflare.php");
-	$CF = new CloudFlare("FIXME");
+	$CF->setHostKey("FIXME");
+
+	if (isset($_POST['action'])) {
+		$action = filter_var($_POST['action'], FILTER_SANITIZE_STRING);
+		return $CF->submitAction($action,$_POST);
+	}
 	
-	if (@$_POST['action'] == "user_create"){
-		return $CF->userCreate($_POST["cloudflare_email"], $_POST["cloudflare_pass"], @$_POST["cloudflare_username"], @$_POST["unique_id"]);
+}
+
+
+function createForm($id) {
+	global $CF;
+	
+	if (!isset($CF->actions[$id])){
+		echo '<h4>Error: '.$id.' not defined.</h4>'; 
+		return false; //fail
 	}
-	elseif (@$_POST['action'] == "user_lookup" && isset($_POST["cloudflare_email"])){
-		return $CF->userLookup($_POST["cloudflare_email"], @$_POST["unique_id"]);
+
+	$title=isset($CF->actions[$id]['title']) ? $CF->actions[$id]['title'] : '';
+	$description=isset($CF->actions[$id]['description']) ? $CF->actions[$id]['description'] : '';
+	$required=isset($CF->actions[$id]['required']) ? $CF->actions[$id]['required'] : [];
+	$optional=isset($CF->actions[$id]['optional']) ? $CF->actions[$id]['optional'] : [];
+	$fields=array_merge($required,$optional);
+
+	echo '<div class="row" id="'.$id.'">';
+	if ($title) echo '<h4>'.$title.'</h4>';
+	if ($description) echo '<p>'.$description.'</p>';
+
+	echo '<form id="'.$id.'_form" class="col s12" method="POST"><div class="row">';
+
+	foreach ($fields as $k) {
+		$type=$CF->fields[$k]['type'];
+		$label=$CF->fields[$k]['label'];
+		$tooltip=isset($CF->fields[$k]['tooltip']) ? $CF->fields[$k]['tooltip'] : '';
+		$validateStr=isset($required[$k]) ? 'validate' : '';
+		echo '<div class="input-field col s12">';
+		if ($tooltip) {
+			echo '<input name="'.$k.'" type="'.$type.'" data-tooltip="'.$tooltip.'" data-position="bottom" class="tooltipped '.$validateStr.'">';
+		}
+		else {
+			echo '<input name="'.$k.'" type="'.$type.'" class=" '.$validateStr.'">';
+		}
+		echo '<label for="'.$k.'">'.$label.'</label>';
+		echo '</div>';
 	}
+	echo '<div class="input-submit col s12"><button class="btn waves-effect waves-light" type="submit" name="action" value="'.$id.'">Submit</button></div>';
+	echo '</div></form></div>';
 }
 
 ?>
 
-	<div id="main" class="card" style="max-width:700px;margin:0 auto;">
+	<div id="main" class="card"> 
 
 	<div class="card-content">
-		<h2>Cloudflare Partner Management</h2>
+		<h2>Cloudflare Hosting Provider Manager</h2>
 		<ul class="collapsible" id="results">
 			<li><div style="padding:1em;">
 				<pre>
@@ -56,107 +98,27 @@ function getSubmitResults() {
 
 	<div class="card-tabs">
 		<ul id="tabselect" class="tabs tabs-fixed-width">
-			<li class="tab"><a href="#user_lookup" class="active">User Lookup</a></li>
-			<li class="tab"><a href="#user_create">User Create</a></li>
-			<li class="tab"><a href="#user_auth">User Auth</a></li>
-			<li class="tab"><a href="#zone_lookup">Zone Lookup</a></li>
+			<?php
+				$c=0;
+				foreach ($CF->actions as $k=>$v) {
+					echo '<li class="tab"><a href="#'.$k.'" class="'.($c==0 ? "active" : "").'">'.$v["title"].'</a></li>';
+					$c++;
+				}
+			?>
 		</ul>
 	</div>
 	<div class="card-content">
-		<div class="row" id="user_lookup">
-			<h4>User Lookup</h4>
-			<p>Look a user by 'cloudflare_email' or by a previosly assigned 'unique_id'.</p>
-			<form id="user_lookup_form" class="col s12" method="POST">
-				<div class="row">
-					<div class="input-field col s12">
-						<input placeholder="Cloudflare Email" name="cloudflare_email" type="email" class="validate">
-						<label for="cloudflare_email">Cloudflare Email</label>
-					</div>
-					<div class="input-field col s6">
-						<input placeholder="Unique ID" name="unique_id" type="text">
-						<label for="unique_id">Unique ID (optional)</label>
-					</div>
-					<div class="input-submit col s12">
-						<button class="btn waves-effect waves-light" type="submit" name="action" value="user_lookup">Submit</button>
-					</div>
-				</div>
-			</form>
-		</div>
-
-		<div class="row" id="user_create">
-			<h4>User Create</h4>
-			<p>Create a CloudFlare account for a user. Register you as the host provider.</p>
-			<form id="user_create_form" class="col s12" method="POST">
-				<div class="row">
-					<div class="input-field col s12">
-						<input placeholder="Cloudflare Email" name="cloudflare_email" type="email" class="validate">
-						<label for="cloudflare_email">Cloudflare Email</label>
-					</div>
-					<div class="input-field col s12">
-						<input name="cloudflare_pass" type="password" class="validate">
-						<label for="cloudflare_email">Cloudflare Password</label>
-					</div>
-					<div class="input-field col s6">
-						<input placeholder="Cloudflare Username" name="cloudflare_username" type="text">
-						<label for="cloudflare_username">Cloudflare Username (optional)</label>
-					</div>
-					<div class="input-field col s6">
-						<input placeholder="Unique ID" name="unique_id" type="text">
-						<label for="unique_id">Unique ID (optional)</label>
-					</div>
-					<div class="input-submit col s12">
-						<button class="btn waves-effect waves-light" type="submit" name="action">Submit</button>
-					</div>
-				</div>
-			</form>
-		</div>
-
-		<div class="row" id="user_auth">
-			<h4>User Authorize</h4>
-			<p>Authorize you as the host provider for an existing user.</p>
-			<form id="user_auth_form" class="col s12" method="POST">
-				<div class="row">
-					<div class="input-field col s12">
-						<input placeholder="Cloudflare Email" name="cloudflare_email" type="email" class="validate">
-						<label for="cloudflare_email">Cloudflare Email</label>
-					</div>
-					<div class="input-field col s12">
-						<input name="cloudflare_pass" type="password" class="validate">
-						<label for="cloudflare_email">Cloudflare Password</label>
-					</div>
-					<div class="input-field col s6">
-						<input placeholder="Unique ID" name="unique_id" type="text">
-						<label for="unique_id">Unique ID (optional)</label>
-					</div>
-					<div class="input-submit col s12">
-						<button class="btn waves-effect waves-light" type="submit" name="action" value="user_auth">Submit</button>
-					</div>
-				</div>
-			</form>
-		</div>
-
-		<div class="row" id="zone_lookup">
-			<h4>Zone Lookup</h4>
-			<p>Lookup a Zone by 'user_key' and 'zone_name'.</p>
-			<form id="zone_lookup_form" class="col s12" method="POST">
-				<div class="row">
-					<div class="input-field col s12">
-						<input placeholder="Zone Name" name="zone_name" type="text">
-						<label for="zone_name">Zone Name</label>
-					</div>
-					<div class="input-field col s12">
-						<input name="user_key" type="password" class="validate">
-						<label for="user_key">User Key</label>
-					</div>
-					<div class="input-submit col s12">
-						<button class="btn waves-effect waves-light" type="submit" name="action" value="zone_lookup">Submit</button>
-					</div>
-				</div>
-			</form>
-		</div>
+		<?php
+			foreach ($CF->actions as $k=>$v) {
+				createForm($k);
+			}
+		?>
 	</div>
 	<script>
-		var instance = M.Tabs.init(document.getElementById('tabselect'),{});
+		document.addEventListener('DOMContentLoaded', function() {
+			var tooltips = M.Tooltip.init(document.querySelectorAll('.tooltipped'), {});
+			var tabselect = M.Tabs.init(document.getElementById('tabselect'),{});
+		});
 	</script>
 
 </body>
